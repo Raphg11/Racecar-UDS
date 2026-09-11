760e436f777f193a5258b038e792c76424759b16
MathisBelleau <mathis.belleau@gmail.com>
2026-09-11 15:31:44 -0400

    code Vehicule_tracker

--- Racecar/ros2_ws/src/racecar-ros2/racecar_beacon/racecar_beacon/vehicle_tracker.py
+++ Racecar/ros2_ws/src/racecar-ros2/racecar_beacon/racecar_beacon/vehicle_tracker.py
@@ -13,7 +13,21 @@ NOTES:
 
 def main():
     # TODO: Implement the PositionBroadcast client here.
-    pass
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    s.bind(('',5001))
+
+    while True:
+        data, address = s.recvfrom(16)
+
+        x, y, z, theta, robotid = unpack('fffI', data)
+
+        print(address[0], robotid, x, y, z, theta)
+
+    s.close()
 
 
 if __name__ == "__main__":
